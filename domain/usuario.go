a318efa462130eb1cf6ffce98fe1e94a0d9c5a17
Eliot Rabanal <lino.rabanal@gestionysistemas.com>
2025-04-10 16:36:00 -0500

    .

--- domain/usuario.go
+++ domain/usuario.go
@@ -5,10 +5,10 @@ import (
 )
 
 type USUARIO struct {
-	ID     int64  `json:"id" gorm:"type:int;not null;primary_key;autoIncrement"`
-	Correo string `json:"correo" gorm:"type:varchar(200);"`
-	/*Nombre    string `json:"nombre" gorm:"type:char(200);not null"`
-	Apellidos string `json:"apellidos" gorm:"type:char(200);not null"`*/
+	ID        int64  `json:"id" gorm:"type:int;not null;primary_key;autoIncrement"`
+	Correo    string `json:"correo" gorm:"type:varchar(200);"`
+	Nombre    string `json:"nombre" gorm:"type:varchar(200);not null"`
+	Apellidos string `json:"apellidos" gorm:"type:varchar(200);not null"`
 }
 
 func (u *USUARIO) BeforeCreate(tx *gorm.DB) (err error) {
