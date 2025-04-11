0fb3c5f774d85b374a0d15d0acaa769f146a8d1b
Eliot Rabanal <lino.rabanal@gestionysistemas.com>
2025-04-11 12:00:20 -0500

    .

--- domain/base.go
+++ domain/base.go
@@ -2,12 +2,9 @@ package domain
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type BASE struct {
-	gorm.Model
 	ID                   int64     `json:"id" gorm:"type:int;not null;primary_key;autoIncrement"`
 	Es_Activo            bool      `json:"es_activo" gorm:"type:datetime;not null;"`
 	Usuario_Creacion     int64     `json:"usuario_creacion" gorm:"type:int;null;"`
